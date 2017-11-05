c80c93c61d650f8d4da143f773f97c89da3aec1f
Jorge Septien <septien.jorge@gmail.com>
2017-11-05 10:18:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# CarND-Extended-Kalman-Filters-P1
+CarND Extended Kalman Filters Project
+
+Performs sensor fusion from Lidar and Radar measurements and uses Extended Kalman Filter methods for predicting and updating and objects position.
+
+## Build Instructions
+
+From build, run `cmake .. && make` and execute with `./ExtendedKF`.
---
+++ build/CMakeFiles/UnscentedKF.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jseptien/projects/self-driving/term2/CarND-Unscented-Kalman-Filter-P2/src/main.cpp" "/home/jseptien/projects/self-driving/term2/CarND-Unscented-Kalman-Filter-P2/build/CMakeFiles/UnscentedKF.dir/src/main.cpp.o"
+  "/home/jseptien/projects/self-driving/term2/CarND-Unscented-Kalman-Filter-P2/src/tools.cpp" "/home/jseptien/projects/self-driving/term2/CarND-Unscented-Kalman-Filter-P2/build/CMakeFiles/UnscentedKF.dir/src/tools.cpp.o"
+  "/home/jseptien/projects/self-driving/term2/CarND-Unscented-Kalman-Filter-P2/src/ukf.cpp" "/home/jseptien/projects/self-driving/term2/CarND-Unscented-Kalman-Filter-P2/build/CMakeFiles/UnscentedKF.dir/src/ukf.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
